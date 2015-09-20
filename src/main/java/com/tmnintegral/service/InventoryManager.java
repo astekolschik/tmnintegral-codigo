1d789a8f51195a6026933e566c229007218d207b
Agus <agussteko@gmail.com>
2015-09-20 19:26:23 -0300

    InventoryManager

---
+++ src/main/java/com/tmnintegral/service/InventoryManager.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.tmnintegral.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.tmnintegral.domain.TipoEquipo;
+import com.tmnintegral.repository.TipoEquipoDao;
+
+/**
+ * @author Agustina
+ *
+ */
+@Component
+public class InventoryManager implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private TipoEquipoDao tipoEquipoDao;
+
+	/**
+	 * Devuelve la lista de los tipos de equipo
+	 * @return
+	 */
+	public List<TipoEquipo> getTipoEquiposList(){
+		return this.tipoEquipoDao.getTipoEquiposList();
+	}
+	
+	
+}
